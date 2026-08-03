59d7b3774cea53707f02c922c077b16625a4f30b
Björn Hägglund <bjorn.hagglund@nav.no>
2026-08-03 14:00:44 +0200

    some sorting

--- src/main/kotlin/no/nav/platforce/tool/ignore/IgnoredRepositoriesStore.kt
+++ src/main/kotlin/no/nav/platforce/tool/ignore/IgnoredRepositoriesStore.kt
@@ -14,6 +14,7 @@ class IgnoredRepositoriesStore(
             repositories =
                 PostgresDatabase
                     .getIgnoredRepositories(userId)
+                    .sorted()
                     .toMutableList(),
         )
 
