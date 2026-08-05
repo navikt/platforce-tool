8780b92dbc04cf93a55aa7795bf474c51d91de3a
Björn Hägglund <bjorn.hagglund@nav.no>
2026-08-05 12:57:44 +0200

    change wrapper updateer to keep escape before colon

--- src/main/kotlin/no/nav/platforce/tool/dependencies/GradleWrapperUpdater.kt
+++ src/main/kotlin/no/nav/platforce/tool/dependencies/GradleWrapperUpdater.kt
@@ -15,12 +15,12 @@ class GradleWrapperUpdater {
                 val target = f.targetVersion ?: return@forEach
 
                 updated =
-                    updated.replace(
-                        Regex(
-                            """distributionUrl=.*gradle-([0-9.]+)-bin\.zip""",
-                        ),
-                        "distributionUrl=https\\://services.gradle.org/distributions/gradle-$target-bin.zip",
-                    )
+                    Regex(
+                        """distributionUrl=.*gradle-[0-9.]+-(bin|all)\.zip""",
+                    ).replace(updated) { match ->
+                        val suffix = match.groupValues[1]
+                        "distributionUrl=https\\://services.gradle.org/distributions/gradle-$target-$suffix.zip"
+                    }
             }
 
         return updated
