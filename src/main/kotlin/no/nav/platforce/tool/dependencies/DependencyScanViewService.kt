ecf01be9444c5176eda9070f991c05aa48545f77
Björn Hägglund <bjorn.hagglund@nav.no>
2026-09-25 14:44:14 +0200

    tracing missing enrichment bug

--- src/main/kotlin/no/nav/platforce/tool/dependencies/DependencyScanViewService.kt
+++ src/main/kotlin/no/nav/platforce/tool/dependencies/DependencyScanViewService.kt
@@ -1,11 +1,24 @@
 package no.nav.platforce.tool.dependencies
 
+import mu.KotlinLogging
+
 class DependencyScanViewService(
     private val dependencyScanCache: DependencyScanCache,
     private val targetSecurityScanner: TargetSecurityScanner,
     private val dependencyScanner: DependencyScanner,
 ) {
+    val log = KotlinLogging.logger { }
+
     fun get(targetState: TargetVersionsState): List<RepositoryDependencyScan> {
+        val securitySnapshot =
+            targetSecurityScanner.get(targetState)
+
+        log.info {
+            "DEPENDENCY VIEW SECURITY: " +
+                "status=${securitySnapshot.status}, " +
+                "hasResult=${securitySnapshot.result != null}"
+        }
+
         val securityResult =
             targetSecurityScanner
                 .get(targetState)
